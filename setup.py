b08a18dc9f3bdcf835d3f064431b0de93fa86556
oldmanbluntz <michael.l.bailey@hotmail.com>
2023-01-13 20:08:10 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/gigibu5/OctoLight"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ["RPi.GPIO"]
+plugin_requires = ["adafruit-blinka"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
